704d55841051e8eae231aea4f483be9491668de0
dineshgitclone <116178552+dineshgitclone@users.noreply.github.com>
2023-09-17 14:24:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -85,7 +85,7 @@ async def start(client, message):
                 btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**You are not in our Sponsored channel given below so you don't get the movie file...\n\nIf you want the movie file, click on the '🍿Join our Sponsored Channel🍿' button below and join our Sponsored channel, then click on the '🔄 Try Again' button below...\n\nThen you will get the movie files...**",
+            text="**❗️Important**\n\nClick on the **'Join our Sponsored Channel'** button below and join our Sponsored channel, then click on the **'🔄 Try Again'** button below...\n\nThen you will get the **movie files ☺️...**",
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode=enums.ParseMode.MARKDOWN
             )
